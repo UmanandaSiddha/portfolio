1890ddb86b74b9207ced9b0d0e4b8f936f2ae19a
Umananda Siddha <umanandasiddha243@gmail.com>
2024-03-17 04:25:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+    return (
+        <div>
+            hello, I am Umananda Siddha
+            I am a Full Stack Developer.
+        </div>
+    )
+}
+
+export default App;
